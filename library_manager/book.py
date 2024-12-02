e401ac9ae2a47e5c274e6d9cdcd37375851f0e0b
Raynece <165816070+Raynece@users.noreply.github.com>
2024-12-02 11:51:39 +0500

    Update book.py

--- library_manager/book.py
+++ library_manager/book.py
@@ -2,11 +2,11 @@ class Book:
     """Класс, представляющий книгу."""
 
     def __init__(self, title: str, author: str, year: int, id: int = None, status: str = "в наличии"):
-        self.id = id  # ID может быть передан или установлен позже
+        self.id = id 
         self.title = title
         self.author = author
         self.year = year
-        self.status = status  # Теперь статус также передается в конструктор
+        self.status = status
 
     def __repr__(self):
         return f"ID: {self.id}, Title: {self.title}, Author: {self.author}, Year: {self.year}, Status: {self.status}"
